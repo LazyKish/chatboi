2bdb050945110c1d8cb1a52221e9a12cac64856c
christian1234t <christianubac789@gmail.com>
2024-12-17 22:23:42 +0800

    added dependencies

--- PyChat/settings.py
+++ PyChat/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   , # Use the correct app name and module path
+    # Use the correct app name and module path
 ]
 
 
